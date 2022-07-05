3e7945923843f89a547317626767c23de03d2ff6
2nd <2nd@dedries-MacBook-Air.local>
2022-07-05 01:31:47 -0400

    save

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // Assignment code here
 
+var letter = "abcdefghijklmnopqrstuvwxyz"
+var symbol = "!@#$%^&*()"
+var number = 1234567890
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  var prompt1 = window.prompt("Would you like your password to include letters?")
+  var prompt2 = window.prompt("Would you like your password to include numbers?")
+  var prompt3 =window.prompt("Would you like your password to include symbols?")
+
+if(prompt1 === yes.toUpperCase()){
+
+}
+  
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
